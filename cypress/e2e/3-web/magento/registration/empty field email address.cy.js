0872bd65d5f24411809f4dd21a68b99cb00a19ae
NabillaSafira <nabillasafira1999@gmail.com>
2025-01-19 10:34:34 +0700

    create an account with empty field email

---
+++ cypress/e2e/3-web/magento/registration/empty field email address.cy.js
@@ -0,0 +1,21 @@
+describe('Create an account', () => {
+  beforeEach(() => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+  })
+
+  it('Create an Account with empty field email address', () => {
+    cy.contains('Create an Account').click()
+    cy.get('#firstname').type("Nabilla")
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('#lastname').type("Safira")
+    .should('have.attr', 'aria-required', 'true'); 
+    cy.get('[name="email"]')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get('#password').type("@Nabilla1234")
+    .should('have.attr', 'aria-required', 'true'); 
+    cy.get('[name="password_confirmation"]').type("@Nabilla1234")
+    .should('have.attr', 'aria-required', 'true'); 
+    cy.get('.action.submit.primary').click()
+    .should('be.visible');
+  })
+})
