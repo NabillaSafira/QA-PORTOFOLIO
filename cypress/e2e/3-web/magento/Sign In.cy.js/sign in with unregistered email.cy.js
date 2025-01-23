2d4e5b46f85baebb5232e4643b51e6b3500dd3ee
NabillaSafira <nabillasafira1999@gmail.com>
2025-01-23 11:07:26 +0700

    add sign in testing

---
+++ cypress/e2e/3-web/magento/Sign In.cy.js/sign in with unregistered email.cy.js
@@ -0,0 +1,19 @@
+import loginPage from "../../../../support/pageObject/loginPage"
+describe('Registration', () => {
+    beforeEach(() => {
+      cy.visit('')
+    })
+  
+    it('Sign in with registered email', () => {
+    cy.contains('Sign In').click()
+    cy.get(loginPage.login_Email).type('nabilla@gmail.com')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get(loginPage.login_pass).type('@Nabilla123')
+    .should('have.attr', 'aria-required', 'true');
+    cy.get(loginPage.login_btn).click()
+    .should('be.visible');
+    
+    cy.get(loginPage.error_login)
+   .should('include.text', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
+    })
+})
---
+++ cypress/support/pageObject/loginPage.js
@@ -0,0 +1,9 @@
+class loginPage{
+ login_Email = '[name="login[username]"]'  
+ login_pass ='#pass'
+ login_btn = '.action.login.primary'
+ error_login = '.message-error'
+
+
+}
+export default new loginPage()
