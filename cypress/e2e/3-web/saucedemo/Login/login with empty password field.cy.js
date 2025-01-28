6786d0fc57664ff709fc1e0f44f00dface9cf081
NabillaSafira <nabillasafira1999@gmail.com>
2025-01-28 11:22:24 +0700

    login wity empty username and password field

--- cypress/e2e/3-web/saucedemo/Login/login with empty password field.cy.js
+++ cypress/e2e/3-web/saucedemo/Login/login with empty password field.cy.js
@@ -1,15 +1,18 @@
 import loginPageSaucedemoCy from "../../../../support/pageObject/saucedemo/loginPageSaucedemo.cy"
-describe('Seccessfully Login', () => {
+describe('login with empty username field', () => {
     beforeEach(() => {
       cy.visit('')
     })
-    it('successfully login', () => {
+    it('login with empty password field', () => {
     cy.get(loginPageSaucedemoCy.name).type('standard_user')
-    .should('be.visible');
-    cy.get(loginPageSaucedemoCy.password).type('secret_sauce')
+    .should('be.visible')
+    cy.get(loginPageSaucedemoCy.password)
     .should('be.visible');
     cy.get(loginPageSaucedemoCy.login_btn)
     .should('be.visible').click(); 
+
+    cy.get('[data-test="error"]')
+    .should('include.text', 'Epic sadface: Password is required');
   
     })
 })
--- cypress/support/pageObject/saucedemo/loginPagesaucedemo.cy.js
+++ cypress/support/pageObject/saucedemo/loginPagesaucedemo.cy.js
@@ -2,5 +2,6 @@ class loginPageSaucedemo{
     name = '#user-name'
     password = '#password'
     login_btn ='[data-test="login-button"]'
+    error_message = '[data-test="error"]'
 }
  export default new loginPageSaucedemo()
