47061f6d4d07d8a9732100848bc4de7a6db1aed2
NabillaSafira <nabillasafira1999@gmail.com>
2025-01-30 11:50:26 +0700

    Add and remove items from the cart and proceed to checkout

--- cypress/support/pageObject/saucedemo/cartPagesaucedemo.cy.js
+++ cypress/support/pageObject/saucedemo/cartPagesaucedemo.cy.js
@@ -2,5 +2,7 @@ class cartPagesaudemo{
     add_backpage = '#add-to-cart-sauce-labs-backpack'
     add_jacket = '#add-to-cart-sauce-labs-fleece-jacket'
     add_tshirt = '[name="add-to-cart-test.allthethings()-t-shirt-(red)"]'
+    cart = '[data-test="shopping-cart-link"]'
+    remove_backpage ='[data-test="remove-sauce-labs-backpack"]'
 }
 export default new cartPagesaudemo()
